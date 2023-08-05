ead256ddbf49f92e16ef74a02c987a9e9310a97d
Steven Shaginyan <SShaginyan@Gmail.com>
2023-08-05 16:23:04 -0700

    Modified positions for Direction enum + added State enum

--- common.py
+++ common.py
@@ -7,4 +7,8 @@ SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 WHITE = (255, 255, 255)
 Point = namedtuple("Point", ['x', 'y'])
-Direction = Enum('Direction', ['UP', 'DOWN', 'LEFT', 'RIGHT'])
+Direction = Enum('Direction', ['UP', 'LEFT', 'DOWN', 'RIGHT'], start=0)
+State = Enum('State', [
+    'DAN_S', 'DAN_R', 'DAN_L', 'DIR_L', 'DIR_R',
+    'DIR_U', 'DIR_D', 'FOO_L', 'FOO_R', 'FOO_U', 'FOO_D'
+], start=0)
